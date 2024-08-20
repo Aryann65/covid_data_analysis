9e4e57ec2da0ad08557afcc61c97b47a2c5d2b11
Aryann65 <117563898+Aryann65@users.noreply.github.com>
2024-08-21 01:30:39 +0530

    Update tableau_project.sql

--- tableau_project.sql
+++ tableau_project.sql
@@ -1,10 +1,3 @@
-/*
-
-Queries used for Tableau Project
-
-*/
-
-
 
 -- 1. 
 
@@ -15,16 +8,6 @@ where continent is not null
 --Group By date
 order by 1,2
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
 
 
 -- 2. 
